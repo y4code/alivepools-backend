99de96a8492183c90ee713b862026e9fca407680
y4code <8910420+y4code@users.noreply.github.com>
2024-02-04 20:24:21 +0000

    app scaffold setup

--- alivepools-backend/__init__.py
+++ alivepools-backend/__init__.py
@@ -1,10 +1,38 @@
-def create_app(config_name):
-    app = Flask(__name__)
-    app.config.from_object(app_config[config_name])
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
-    bcrypt.init_app(app)
-    jwt.init_app(app)
-    CORS(app)
+import os
+from flask import Flask, Blueprint
+from flask_jwt_extended import JWTManager, create_access_token
 
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    app.config['JWT_SECRET_KEY'] = 'your_secret_key_here'
+
+    # Initialize JWTManager
+    jwt = JWTManager(app)
+
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Create a blueprint
+    bp = Blueprint('my_blueprint', __name__)
+
+    # Register the blueprint
+    app.register_blueprint(bp)
+    
     return app
