a448e97b63bff9c019930c4f073bd162c476207f
Sirius <rollingwayne@gmail.com>
2024-02-08 23:20:16 +0000

    update the time of requests, chang 0 to 5.

--- alivepools-backend/domain.py
+++ alivepools-backend/domain.py
@@ -10,13 +10,13 @@ bp = Blueprint('domain', __name__)
 def check_by_domain():
     website = request.args.get("website")
     try:
-        response = requests.get("http://" + website)
-        # If the GET request is successful, the website is available
-        if response.status_code == 200:
-            return jsonify({"message": "Website is available"}), 200
-        else:
-            # If the server returns a status code other than 200, it's considered unavailable for this purpose
-            return jsonify({"message": "Website is not available"}), 200
+        for _ in range(5):
+            response = requests.get("http://" + website)
+            # If the GET request is successful, the website is available
+            if response.status_code == 200:
+                return jsonify({"message": "Website is available"}), 200
+        # If all requests fail, the website is considered not available
+        return jsonify({"message": "Website is not available"}), 200
     except requests.exceptions.RequestException:
         # If the GET request fails (e.g., due to a network problem or invalid URL), the website is considered not found
         return jsonify({"message": "Website not found"}), 200
