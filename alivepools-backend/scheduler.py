be9f73a1a423398790bdc1a2fbc7bf0dc78eb64d
Sirius <rollingwayne@gmail.com>
2024-02-13 16:55:28 +0000

    Update scheduler interval to 60 seconds

--- alivepools-backend/scheduler.py
+++ alivepools-backend/scheduler.py
@@ -5,5 +5,5 @@ from apscheduler.schedulers.background import BackgroundScheduler
 def init_scheduler():
     from .job import execute_jobs
     scheduler = BackgroundScheduler(daemon=True)
-    scheduler.add_job(execute_jobs, 'interval', seconds=5, max_instances=100)
+    scheduler.add_job(execute_jobs, 'interval', seconds=60, max_instances=100)
     scheduler.start()
