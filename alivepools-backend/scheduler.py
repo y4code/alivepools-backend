bbd9f6319998e263ea5ef9da7d23d3b58612f196
Sirius <rollingwayne@gmail.com>
2024-02-19 03:00:22 +0000

    Commented out scheduler job initialization

--- alivepools-backend/scheduler.py
+++ alivepools-backend/scheduler.py
@@ -5,5 +5,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 def init_scheduler():
     from .job import execute_jobs
     scheduler = BackgroundScheduler(daemon=True)
-    scheduler.add_job(execute_jobs, 'interval', seconds=60, max_instances=100)
-    scheduler.start()
+
+    # 注释以启动定时任务
+    # scheduler.add_job(execute_jobs, 'interval', seconds=60, max_instances=100)
+    # scheduler.start()
