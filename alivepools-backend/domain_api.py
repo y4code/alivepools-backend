5f6603a5cc4db582b7ec7e1e9ad0b394e72b52d2
y4code <8910420+y4code@users.noreply.github.com>
2024-03-09 19:25:19 +0000

    fix(start with http[s]): remove htttp[s]

--- alivepools-backend/domain_api.py
+++ alivepools-backend/domain_api.py
@@ -13,11 +13,15 @@ bp = Blueprint("domain_api", __name__)
 @bp.route("/domain", methods=["POST"])
 def check_by_domain():
     website = request.json.get("website")
+    if not website.startswith(("http://", "https://")):
+        website = "http://" + website
     try:
         for _ in range(5):
-            response = requests.get("http://" + website)
+            response = requests.get(website)
             if response.status_code == 200:
                 return response_ok(None)
-        return response_error(None, CODE_WEBSITE_NOT_AVAILABLE, "Website not available")
+
     except requests.exceptions.RequestException:
         return response_error(None, CODE_WEBSITE_NOT_FOUND, "Website not found")
+
+    return response_error(None, CODE_WEBSITE_NOT_AVAILABLE, "Website not available")
