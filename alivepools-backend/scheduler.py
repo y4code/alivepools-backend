d03cbc4b49361497edea55bb0de57fd31fd4bba0
y4code <8910420+y4code@users.noreply.github.com>
2024-03-14 04:24:41 +0000

    fix(scheduler instance): reduce from 5 to 1

--- alivepools-backend/scheduler.py
+++ alivepools-backend/scheduler.py
@@ -5,6 +5,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 def init_scheduler(app):
     scheduler = BackgroundScheduler(daemon=True)
     scheduler.add_job(
-        func=execute_tasks, args=(app,), trigger="interval", seconds=60, max_instances=5
+        func=execute_tasks, args=(app,), trigger="interval", seconds=60, max_instances=1
     )
     scheduler.start()
